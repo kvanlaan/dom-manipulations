22b88c0d1c2e2ca30f48b18036eb88f2b40006e3
Katrina Van Laan <kdvanlaan@gmail.com>
2016-02-19 15:08:52 -0600

    ghpage

--- main.js
+++ main.js
@@ -15,83 +15,125 @@ p.innerText = finNum
 })
 
 
-var p = document.getElementById('myspan');
-span.innerText = span.textContent = 'newtext';
 
 document.querySelector("#color-circle button").addEventListener('click',function(){
   // TASK #3
-  
-  alert('clicked')
+
   var circle = document.querySelector("#circle-bw")
   if (circle.style.background === "black") {
-  	circle.style.backround = "white"
+  	circle.style.background = "white"
   }
   else {
   	circle.style.background = "black"
   }
+
 })
 
 document.querySelector("#blow-up button").addEventListener('click',function(){
-  // TASK #4}
+  // TASK #4
 
 
-var circle = document.querySelector("div .circle-red")
-var circleStyles = window.getComputedStyle(circle)
+var redCircle = document.querySelector(".circle-red")  
 
-var doublePx = function(pxValue) {
-  var doubledInt = parseInt(pxValue) * 2
-  return doubledInt + "px"  
+var redCircleStyles = window.getComputedStyle(redCircle)
+
+var oldCircleWidth = parseInt(redCircleStyles.width)
+redCircle.style.width = (oldCircleWidth * 2) + "px"
+
+var oldCircleHeight = parseInt(redCircleStyles.height)
+redCircle.style.height = (oldCircleHeight * 2) + "px"
+
+if (oldCircleHeight >= 320) {
+  redCircle.style.width = 40 + "px"
+  redCircle.style.height = 40 + "px"
 }
 
-circle.style.width = doublePx(circleStyles.width)
-circle.style.height = doublePx(circleStyles.height)
-
 })
+
 
 /////
 
 
 document.querySelector("#remove button").addEventListener('click',function(){
   // TASK #5
+
+
+  var userList = document.querySelector("#userList")
+  var inactive = document.querySelectorAll(".inactive")
+
+
+var inactivePresent = true
+for (i = 0; i <inactive.length; i++) {
+if (inactivePresent) {
+  userList.removeChild(inactive[i])
+  }
+
+}
 })
 
- Array.prototype.contains = function(el) {
-  return this.indexOf(el) !== -1
-}
-
-var isDone = function(choreElement) {
-  console.log(choreElement.classList)
-  // is "done" in the classList?   
-  for (var i = 0; i < choreElement.classList.length; i ++) {
-    if (choreElement.classList[i] === 'done') {
-      return true
-    }
-  }
-  return false
-}
-
-var choresContainer = document.querySelector("#box5 ul")
-
-var completedChoresUl = document.querySelector('#box5s ul')
-
-var chores = choresContainer.querySelectorAll('li')
-
-for (var i = 0; i < chores.length; i ++) {
-  var choreNode = chores[i]
-  if (isDone(choreNode)) {
-    choresContainer.removeChild(choreNode)
-    completedChoresUl.appendChild(choreNode)
-  }
-}
 
 document.querySelector("#reverse-squares button").addEventListener('click',function(){
   // TASK #6
+
+
+var reverseBox = document.querySelector("#reverse-squares .answer-box")
+var squares = reverseBox.querySelectorAll(".square")
+
+
+for (var i = squares.length; i--;) {
+ 
+  reverseBox.appendChild(squares[i])
+
+}
+  
 })
+    
+
 
 document.querySelector("#pig-latin button").addEventListener('click',function(){
   // TASK #7
+
+var textBox = document.querySelector("#pig-latin .answer-box")
+var text = textBox.querySelectorAll("li")
+
+
+var reverseString = function(input) {
+  var newString = ""
+  for ( var i = input.length - 1; i>=0; i--) {
+    var letter = input[i]
+    newString = newString + letter
+  }
+  return newString
+}
+
+
+for (var i = 0; i < text.length; i ++) {
+                         
+    text[i].textContent = reverseString(text[i].textContent)
+}
+
+
 })
+    
 
 document.querySelector("#cycle-image button").addEventListener('click',function(){
   // TASK #8
+
+var cityImage = document.querySelector("#city-image")
+
+var cityImagesrc= document.getElementById("#city-image").src;
+
+var lastNum = cityImagesrc[cityImagesrc.length-1]
+    var newString = ""
+    for (var i=0; i < cityImagesrc.length-1; i++){
+        var char = cityImagesrc[i]
+        newString = newString + char 
+        }
+
+lastNum = parseInt(lastNum) + 1
+
+var stringNum = lastNum.toString()
+cityImagesrc = newString + stringNum
+
+
 })
--- project-setup.js
+++ project-setup.js
@@ -94,3 +94,5 @@ tasks.forEach(function(t, i){
 })
 
 
+
+
